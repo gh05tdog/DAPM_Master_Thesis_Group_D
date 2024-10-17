75ae30bc343501acca1d6714265399b1affb8a31
ravvnen <phillip.boe.jensen@gmail.com>
2024-10-17 15:31:45 +0200

    repository setup + LIGHT MODE AYAYAY

--- DAPM-Frontend/src/components/lists/OrgList.tsx
+++ DAPM-Frontend/src/components/lists/OrgList.tsx
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getOrganizations } from "../../state_management/selectors/apiSelector.ts";
-import { repositoryThunk } from "../../state_management/slices/apiSlice.ts";
+import { organizationThunk } from "../../state_management/slices/apiSlice.ts";
 import OrganizationCard from "../cards/OrganizationCard.tsx";
 
-const RepoList: React.FC = () => {
+const OrgList: React.FC = () => {
     
     const dispatch = useDispatch();
     const organizations = useSelector(getOrganizations); // Adjust state path as needed
     
     useEffect(()=> {
-        dispatch(repositoryThunk());
+        dispatch(organizationThunk());
     }, [dispatch]);
     
 
@@ -19,7 +19,7 @@ const RepoList: React.FC = () => {
             <h1>Organizations</h1>
             <ul data-qa="organiztionList">
             {
-                organizations?.map((repository) => {
+                organizations?.map((organization) => {
                     return (
                         <OrganizationCard organization={organization} />
                     );
--- DAPM-Frontend/src/components/lists/RepoList.tsx
+++ DAPM-Frontend/src/components/lists/RepoList.tsx
@@ -1,33 +1,45 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getRepositories } from "../../state_management/selectors/apiSelector.ts";
-import { repositoryThunk } from "../../state_management/slices/apiSlice.ts";
+import { getRepositories, getOrganizations } from "../../state_management/selectors/apiSelector.ts";
+import { repositoryThunk, organizationThunk } from "../../state_management/slices/apiSlice.ts";
 import RepositoryCard from "../cards/RepositoryCard.tsx";
+import { Organization } from "../../state_management/states/apiState.ts";
 
-const OrgList: React.FC = () => {
-    
+const RepoList: React.FC = () => {
     const dispatch = useDispatch();
-    const repositories = useSelector(getRepositories); // Adjust state path as needed
     
-    useEffect(()=> {
-        dispatch(repositoryThunk());
+    // Get organizations and repositories from the store
+    const organizations = useSelector(getOrganizations);
+    const repositories = useSelector(getRepositories);
+
+    // Fetch organizations on component mount
+    useEffect(() => {
+        dispatch(organizationThunk());
     }, [dispatch]);
-    
+
+    // Fetch repositories once organizations are available
+    useEffect(() => {
+        if (organizations.length > 0) {
+            try {
+                dispatch(repositoryThunk(organizations));
+            }
+            catch (error) {
+                console.error(error);    
+            }
+        }
+
+    }, [dispatch, organizations]);
 
     return (
         <div>
             <h1>Repositories</h1>
             <ul data-qa="Repositories:">
-            {
-                repositories?.map((repository) => {
-                    return (
-                        <RepositoryCard repository={repository} />
-                    );
-                })
-            }
+                {repositories?.map((repository) => (
+                    <RepositoryCard key={repository.id} repository={repository} />
+                ))}
             </ul>
         </div>
-    )
-}
+    );
+};
 
-export default OrgList;
+export default RepoList;
