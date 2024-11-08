bbd97db6c844918938aa90ba6df50224285bf694
Martin Holme Surlykke <s224793@dtu.dk>
2024-11-08 13:31:21 +0100

    Update backendAPI.tsx

--- DAPM-Frontend/src/services/backendAPI.tsx
+++ DAPM-Frontend/src/services/backendAPI.tsx
@@ -471,8 +471,7 @@ export async function putResource(orgId: string, repId: string, formData: FormDa
 }
 
 export async function putPipeline(orgId: string, repId: string, pipelineData:any){
-    console.log(pipelineData)
-    console.log("Org ="+orgId + " Repo = " + repId)
+
     try {
         const response = await fetch(`${path}/Organizations/${orgId}/repositories/${repId}/pipelines`, {
             method: "POST",
