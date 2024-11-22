a7316ef38d980f9e59980f14edb3251d3c112785
HLLissau <68223372+HLLissau@users.noreply.github.com>
2024-11-22 14:41:40 +0100

    added functionality

--- DAPM-Frontend/src/components/overviews/PipelineOverview.tsx
+++ DAPM-Frontend/src/components/overviews/PipelineOverview.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPipelines } from '../../state_management/selectors/index.ts';
 import { useNavigate } from 'react-router-dom';
+import Spinner from '../cards/SpinnerCard.tsx';
+
 import { addNewPipeline, setActivePipeline } from '../../state_management/slices/pipelineSlice';
 import { getOrganizations, getRepositories } from "../../state_management/selectors/apiSelector.ts";
 import { pipelineThunk } from "../../state_management/slices/pipelineSlice.ts"
@@ -11,20 +13,23 @@ import { PipelineData } from "../../state_management/states/pipelineState.ts";
 
 const MainContent: React.FC = () => {
     const dispatch = useDispatch();
-
+    const [loading, setLoading] = useState<boolean>(true);
     const organizations: Organization[] = useSelector(getOrganizations);
     const repositories: Repository[] = useSelector(getRepositories);
     const pipelines: PipelineData[] = useSelector(getPipelines);
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (repositories.length > 0) {
+        if (repositories && repositories.length > 0) {
             try {
                 dispatch(pipelineThunk({ organizations, repositories }));
             } catch (error) {
                 console.log(error);
+            } finally{
+                setLoading(false);
             }
         }
+        
     }, [dispatch, repositories]);
 
     const navigateToPipeline = (id: string) => {
@@ -32,6 +37,14 @@ const MainContent: React.FC = () => {
         dispatch(setActivePipeline(id));
         navigate(`/pipeline/${id}`);
     };
+
+    if (loading) {
+        return (
+            <div>
+                <Spinner />
+             </div>
+        )
+    }
 
     return (
         <Box
