fbb84dbd02c1130370c9de7e8c254f9b80a3f216
HLLissau <hneltrup@hotmail.com>
2024-12-19 13:38:27 +0100

    setting environment in environment

--- E2E UI test/features/tests/PipelineOverview/PipelineOverviewSteps.js
+++ E2E UI test/features/tests/PipelineOverview/PipelineOverviewSteps.js
@@ -8,9 +8,11 @@ const expect = require('chai').expect;
 
 
 Given('I open the pipelineOverview page', async function () {
-	await this.page.goto(environments.local.url);
-	await utils.delay(500);
-	let mainUrl = await this.page.url();
+	const currentPage = (new PipelinePage(this.page));
+	await currentPage.page.goto(environments[environments.selected].url);
+	await currentPage.contains("mainWindow");
+	await utils.delay(1000);
+	let mainUrl = await currentPage.page.url();
 	expect(mainUrl).to.contain("/user")
 });
 Then('the pipelineOverview containst {string}',async function (string) {
